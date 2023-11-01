804dbb80427570eeff21f2d01ee0d17431266f45
satyam <satyampayal10@gmail.com>
2023-11-01 19:18:27 +0530

    01/11/23

---
+++ Employee.java
@@ -0,0 +1,16 @@
+public class Employee{
+    // instance data memeber
+    int id;
+    String name;
+    double salery;
+    {
+        System.out.println("Pre constructor call (int block)");
+    }
+    Employee(){
+        System.out.println("Contructor call");
+    }
+
+    public static void main(String[] args) {
+        Employee amit=new Employee();
+    }
+}
